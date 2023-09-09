6f77207292208acb2d632ce204a92e7b810fce6c
LeanLepra <leandro_nob2@hotmail.com>
2023-09-09 15:20:21 -0300

    server listo

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Todo listo')
+});
+
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+});
