1342d68d9db5bf1ed7af31c226a07a343a34bd63
LeanLepra <leandro_nob2@hotmail.com>
2023-09-10 01:08:48 -0300

    se agrego validacion

--- src/models/schemas/foro.schema.js
+++ src/models/schemas/foro.schema.js
@@ -8,7 +8,21 @@ export const createForoSchema = [
         .isString().withMessage('Debe ser string')
         .notEmpty().withMessage('No debe ser vacío'),
     body('image')
-        .isString().withMessage('Debe ser string')
+        .isURL().withMessage('Debe ser URL')
         .notEmpty().withMessage('No debe ser vacío'),
 ]
 
+export const editForoSchema = [
+    body('title')
+        .optional()
+        .isString().withMessage('Debe ser string')
+        .notEmpty().withMessage('No debe ser vacío'),
+    body('content')
+        .optional()
+        .isString().withMessage('Debe ser string')
+        .notEmpty().withMessage('No debe ser vacío'),
+    body('image')
+        .optional()
+        .isURL().withMessage('Debe ser URL')
+        .notEmpty().withMessage('No debe ser vacío'),
+]
--- src/routes/foro.routes.js
+++ src/routes/foro.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { ctrlCreateForo, ctrlDeleteForo, ctrlGetForo, ctrlUpdateForo } from "../controllers/foro.controllers.js";
-import { createForoSchema } from "../models/schemas/foro.schema.js";
+import { createForoSchema, editForoSchema } from "../models/schemas/foro.schema.js";
 import { validator } from "../middlewares/validator.js"
+import { ForoModel } from "../models/Foro.js";
 
 const foroRouter = Router();
 
 foroRouter.get('/api/foro', ctrlGetForo);
 
-foroRouter.post('/api/foro', ctrlCreateForo);
+foroRouter.post('/api/foro', createForoSchema, validator, ctrlCreateForo);
 
-foroRouter.put('/api/foro/:id',ctrlUpdateForo);
+foroRouter.put('/api/foro/:id', editForoSchema, validator, ctrlUpdateForo);
 
 foroRouter.delete('/api/foro/:id',ctrlDeleteForo);
 
