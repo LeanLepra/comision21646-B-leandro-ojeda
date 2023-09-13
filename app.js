9fdd4667d34db2e2c120c4ef4054f35f5eb0bb41
LeanLepra <leandro_nob2@hotmail.com>
2023-09-12 23:53:07 -0300

    se agregó morgan y helmet que me había olvidado

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { foroRouter } from './src/routes/foro.routes.js';
 import { startDb } from './src/config/database.js';
 import path from 'node:path';
 import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +14,10 @@ const app = express();
 
 app.use(express.json())
 app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet({
+    contentSecurityPolicy: false
+}))
 
 app.use(express.static(path.join(__dirname, 'src', 'public')));
 
